cd6c4584abb10579f9853e91a3e888d63b8a0d03
Nischal Lamichhane <lamichhanen96@gmail.com>
2025-03-11 07:48:57 +0545

    updates

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import Card from "@/components/dashboard/Card";
+import { DollarSign, Package, TrendingUp, TriangleAlert } from "lucide-react";
+import React from "react";
 
 const Dashboard = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+	return (
+		<div className='px-7 mt-6'>
+			<h1 className=' text-xl font-bold'>Dashboard</h1>
+			<div className='  mt-7 grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-3'>
+				<Card
+					title='Total items'
+					icon={<Package className='size-5' />}
+					quantity='100'
+					subtext='+2.5% more than last month'
+				/>
+				<Card
+					title='Low stock items'
+					icon={<TriangleAlert className='text-yellow-500 size-5' />}
+					quantity='100'
+					subtext='+3 since last week'
+				/>
+				<Card
+					title='Total value'
+					icon={<DollarSign className='size-5 text-emerald-600' />}
+					quantity='Rs. 1,00,0203'
+					subtext='+20.1% from last month'
+				/>
+				<Card
+					title='Inventory Turnover'
+					icon={<TrendingUp className='size-5' />}
+					quantity='100'
+					subtext='+2.5% more than last month'
+				/>
+			</div>
+		</div>
+	);
+};
 
-export default Dashboard
+export default Dashboard;
---
+++ src/components/dashboard/Card.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface CardProps {
+	title: string;
+	icon: React.ReactNode;
+	quantity: string;
+	subtext: string;
+}
+
+const Card = ({ title, icon, quantity, subtext }: CardProps) => {
+	return (
+		<div className='min-h-[70px] w-full rounded-md border px-4 py-7 '>
+			<div className='flex justify-between items-center'>
+				<p className='font-medium text-sm text-secondary-foreground select-none'>
+					{title}
+				</p>
+				<span className='text-secondary-foreground'>{icon}</span>
+			</div>
+			<h4 className='text-3xl mt-2 font-semibold'>{quantity}</h4>
+			<p className='text-[12px] text-secondary-foreground'>{subtext}</p>
+		</div>
+	);
+};
+
+export default Card;
